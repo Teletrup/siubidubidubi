5c7d5b3a2628a07487d96de61cc558024944c032
Teletrup <saletrapotasowatosyf@gmail.com>
2022-08-19 09:34:07 +0200

    jomaha! jomaso!

--- script.js
+++ script.js
@@ -2,21 +2,31 @@
 function printMousePos(event) {
   x = event.clientX - 10;
   y = event.clientY - 10;
-  w = 500;
-  h = 500 * Math.sqrt(3) / 2;
   m = (h - y) / Math.sqrt(3);
   a = 1 - (x + m) / w;
   b = 1 - y/h;
   c = 1 - (w - x + m) / w;
+	display();
+}
+
+function display() {
   out.innerHTML =
     "x: " + x + "<br>" +
     "y: " + y + "<br>" +
-    "szeri lejdi: " + a + "<br>" +
+    "szeri szeri lejdi: " + a + "<br>" +
     "jomaha: " + b + "<br>" +
-    "siubidubi: " + c + "<br>";
+    "siubidubidubi: " + c + "<br>";
 }
 
 function init() {
+  w = 500;
+  h = 500 * Math.sqrt(3) / 2;
+	x = w;
+	y = 0;
+	a = 0;
+	b = 0;
+	c = 1;
+	display();
   box = document.getElementById("box");
   out = document.getElementById("out");
   box.addEventListener("click", printMousePos, false);
